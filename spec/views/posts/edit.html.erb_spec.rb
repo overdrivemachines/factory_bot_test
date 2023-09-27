82a32b9c143b45656a1254af08d86ecc86d71070
Dipen Chauhan <get.dipen@gmail.com>
2023-09-27 12:28:32 -0700

    testing edit form

--- spec/views/posts/edit.html.erb_spec.rb
+++ spec/views/posts/edit.html.erb_spec.rb
@@ -1,28 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe "posts/edit", type: :view do
-  let(:post) {
-    Post.create!(
-      title: "MyString",
-      content: "MyText",
-      user: nil
-    )
-  }
+RSpec.describe 'posts/edit', type: :view do
+  let(:post) do
+    FactoryBot.create(:post)
+  end
 
   before(:each) do
     assign(:post, post)
   end
 
-  it "renders the edit post form" do
+  it 'renders the edit post form' do
     render
 
-    assert_select "form[action=?][method=?]", post_path(post), "post" do
+    assert_select 'form[action=?][method=?]', post_path(post), 'post' do
+      assert_select 'input[name=?]', 'post[title]'
 
-      assert_select "input[name=?]", "post[title]"
+      assert_select 'textarea[name=?]', 'post[content]'
 
-      assert_select "textarea[name=?]", "post[content]"
-
-      assert_select "input[name=?]", "post[user_id]"
+      assert_select 'input[name=?]', 'post[image]'
     end
   end
 end
