99b6550611ff5c6cf4a541da4f2c0c3c0bad78bc
Dipen Chauhan <get.dipen@gmail.com>
2023-09-27 13:08:00 -0700

    testing presence of image/file field

--- spec/views/posts/new.html.erb_spec.rb
+++ spec/views/posts/new.html.erb_spec.rb
@@ -2,23 +2,19 @@ require 'rails_helper'
 
 RSpec.describe "posts/new", type: :view do
   before(:each) do
-    assign(:post, Post.new(
-      title: "MyString",
-      content: "MyText",
-      user: nil
-    ))
+    assign(:post, FactoryBot.build(:post))
   end
 
   it "renders new post form" do
     render
 
     assert_select "form[action=?][method=?]", posts_path, "post" do
-
       assert_select "input[name=?]", "post[title]"
 
       assert_select "textarea[name=?]", "post[content]"
 
-      assert_select "input[name=?]", "post[user_id]"
+      # Check for image file field
+      assert_select "input[name=?]", "post[image]"
     end
   end
 end
--- spec/views/posts/show.html.erb_spec.rb
+++ spec/views/posts/show.html.erb_spec.rb
@@ -1,18 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "posts/show", type: :view do
+  # image url
+  include Rails.application.routes.url_helpers
   before(:each) do
-    assign(:post, Post.create!(
-      title: "Title",
-      content: "MyText",
-      user: nil
-    ))
+    @post = assign(:post, FactoryBot.create(:post))
   end
 
   it "renders attributes in <p>" do
     render
-    expect(rendered).to match(/Title/)
-    expect(rendered).to match(/MyText/)
-    expect(rendered).to match(//)
+    expect(rendered).to match(/#{Regexp.escape(@post[:title])}/)
+    expect(rendered).to match(/#{Regexp.escape(@post[:content])}/)
+  end
+
+  it "renders the post image" do
+    render
+    assert_select "img[src=?]", rails_blob_path(@post.image, only_path: true), count: 1
   end
 end
